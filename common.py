442f32a734569120939850f8bec2276fb000c0e2
mstange <max_vincent.stange@mailbox.tu-dresden.de>
2022-05-13 23:45:30 +0200

    data preparation fully described

--- common.py
+++ common.py
@@ -2,18 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def array_division(a, b):
+    """This function devides to arrays and enforces 0/0=0"""
     ratio = []
     for i in range(len(a)):
-        if b[i] == 0:
+        if a[i] == 0:
             ratio.append(0)
         else:
             ratio.append(a[i]/b[i])
     return np.array(ratio)
 
 def plot_hist(variable, data_frame_signal, data_frame_background):
+    """This function plots the normalized signal and background distribution for a given variable"""
     total_signal = sum(data_frame_signal.weight)
     total_background = sum(data_frame_background.weight)
-    bin_center = [(variable['bins'][i] + variable['bins'][i + 1]) / 2. for i in range(len(variable['bins']) - 1)]
 
     # Set up subplots
     fig = plt.figure(figsize=(7, 7))
@@ -21,29 +22,45 @@ def plot_hist(variable, data_frame_signal, data_frame_background):
     axes = gs.subplots(sharex=True, sharey=False)
 
     # Main plot
-    hist_signal = axes[0].hist(data_frame_signal[variable['variable']],
-                               weights=data_frame_signal.weight / total_signal,
-                               bins=variable['bins'],
-                               label='signal',
-                               histtype='step')
-    hist_background = axes[0].hist(data_frame_background[variable['variable']],
-                                   weights=data_frame_background.weight / total_background,
-                                   bins=variable['bins'],
-                                   label='background',
+    if 'binning' in variable:
+        binning = variable['binning']
+        hist_signal = axes[0].hist(data_frame_signal[variable['variable']],
+                                   weights=data_frame_signal.weight / total_signal,
+                                   bins=binning,
+                                   label='signal',
                                    histtype='step')
+        hist_background = axes[0].hist(data_frame_background[variable['variable']],
+                                       weights=data_frame_background.weight / total_background,
+                                       bins=binning,
+                                       label='background',
+                                       histtype='step')
+    else:
+        hist_signal = axes[0].hist(data_frame_signal[variable['variable']],
+                                   weights=data_frame_signal.weight / total_signal,
+                                   label='signal',
+                                   histtype='step')
+        hist_background = axes[0].hist(data_frame_background[variable['variable']],
+                                       weights=data_frame_background.weight / total_background,
+                                       label='background',
+                                       histtype='step')
+        binning = hist_signal[1]
 
+    bin_center = [(binning[i] + binning[i + 1]) / 2. for i in range(len(binning) - 1)]
     # Ratio plot
     axes[1].hist(bin_center,
                  weights=array_division(hist_signal[0], (hist_signal[0] + hist_background[0])),
-                 bins=variable['bins'],
+                 bins=binning,
                  histtype='step')
     
     # Style
-    plt.xlim([variable['bins'][0], variable['bins'][-1]])
+    plt.xlim([binning[0], binning[-1]])
     axes[0].set_title('Signal and background {} distribution'.format(variable['variable']))
     axes[0].set(ylabel='event fraction')
     axes[0].legend()
-    axes[1].set(xlabel=variable['xlabel'], ylabel='normed S/(S+B)')
+    if 'xlabel' in variable:
+        axes[1].set(xlabel=variable['xlabel'], ylabel='normed S/(S+B)')
+    else:
+        axes[1].set(xlabel=variable['variable'], ylabel='normed S/(S+B)')
     axes[1].set_ylim([0, 1.4])
     axes[1].axhline(y=0.5, color='k', linestyle='-', linewidth=0.7)
 
