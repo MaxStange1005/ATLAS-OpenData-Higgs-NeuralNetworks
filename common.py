d2e0fda83d63b2cb11004d446893dbd4c0fc11d5
mstange <max_vincent.stange@mailbox.tu-dresden.de>
2022-07-02 16:56:43 +0200

    roc curve

--- common.py
+++ common.py
@@ -26,10 +26,70 @@ def print_progressbar(value, maximum):
     print(progressbar, end='\r')
 
 
-def plot_hist(variable, data_frame_signal, data_frame_background):
+def plot_hist_sum(variable, input_data_frames):
+    """This function plots the sum off the data frames for a given variable"""
+    fig = plt.figure(figsize=(7, 7))
+    
+    # Order to plot
+    process_order = ['llll', 'Zee', 'Zmumu', 'ttbar_lep', 'VBFH125_ZZ4lep', 'WH125_ZZ4lep', 'ZH125_ZZ4lep', 'ggH125_ZZ4lep']
+    # Colors of processes
+    process_color = {
+        'llll': 'blue',
+        'Zee': 'blueviolet',
+        'Zmumu': 'purple',
+        'ttbar_lep': 'green',
+        'VBFH125_ZZ4lep': 'gold',
+        'WH125_ZZ4lep': 'orange',
+        'ZH125_ZZ4lep': 'sienna',
+        'ggH125_ZZ4lep': 'red'
+    }
+    
+    # Extract the histogram values 
+    labels = []
+    events = []
+    weights = []
+    colors = []
+    for process in process_order:
+        if process not in input_data_frames:
+            continue
+        values = input_data_frames[process]
+        labels.append(process)
+        events.append(np.array(values[variable['variable']]))
+        weights.append(values['totalWeight'])
+        colors.append(process_color[process])
+    
+    # Create the histogram
+    if 'binning' in variable:
+        hist_signal = plt.hist(events,
+                               weights=weights,
+                               bins=variable['binning'],
+                               label=labels,
+                               color=colors,
+                               stacked=True)
+            
+    else:
+        hist_signal = plt.hist(events,
+                               weights=weights,
+                               label=labels,
+                               color=colors,
+                               stacked=True)
+    # Style
+    plt.xlim(hist_signal[1][0], hist_signal[1][-1])
+    plt.ylim(bottom=0)
+    if 'xlabel' in variable:
+        plt.xlabel(variable['xlabel'])
+    else:
+        plt.xlabel(variable['variable'])
+    plt.ylabel('Events')
+    plt.savefig('plots/hist_{}.pdf'.format(variable['variable']))
+    plt.legend()
+    plt.show()
+
+
+def plot_normed_signal_vs_background(variable, data_frame_signal, data_frame_background):
     """This function plots the normalized signal and background distribution for a given variable"""
-    total_signal = sum(data_frame_signal.weight)
-    total_background = sum(data_frame_background.weight)
+    total_signal = sum(data_frame_signal.totalWeight)
+    total_background = sum(data_frame_background.totalWeight)
 
     # Set up subplots
     fig = plt.figure(figsize=(7, 7))
@@ -40,22 +100,22 @@ def plot_hist(variable, data_frame_signal, data_frame_background):
     if 'binning' in variable:
         binning = variable['binning']
         hist_signal = axes[0].hist(data_frame_signal[variable['variable']],
-                                   weights=data_frame_signal.weight / total_signal,
+                                   weights=data_frame_signal.totalWeight / total_signal,
                                    bins=binning,
                                    label='signal',
                                    histtype='step')
         hist_background = axes[0].hist(data_frame_background[variable['variable']],
-                                       weights=data_frame_background.weight / total_background,
+                                       weights=data_frame_background.totalWeight / total_background,
                                        bins=binning,
                                        label='background',
                                        histtype='step')
     else:
         hist_signal = axes[0].hist(data_frame_signal[variable['variable']],
-                                   weights=data_frame_signal.weight / total_signal,
+                                   weights=data_frame_signal.totalWeight / total_signal,
                                    label='signal',
                                    histtype='step')
         hist_background = axes[0].hist(data_frame_background[variable['variable']],
-                                       weights=data_frame_background.weight / total_background,
+                                       weights=data_frame_background.totalWeight / total_background,
                                        label='background',
                                        histtype='step')
         binning = hist_signal[1]
@@ -84,5 +144,5 @@ def plot_hist(variable, data_frame_signal, data_frame_background):
     axes[1].text(0.01, 0.95, 'Non-overlap = {}'.format(np.round(non_overlap, 4)),
                  verticalalignment='top', transform=axes[1].transAxes)
 
-    plt.savefig('plots/investigate_{}.pdf'.format(variable['variable']))
+    plt.savefig('plots/normed_signal_vs_background_{}.pdf'.format(variable['variable']))
     plt.show()
